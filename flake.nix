3e1fdc524861677e0b8e798c43548df2c1c36b20
sharp0802 <sharp0802.business@gmail.com>
2026-07-11 19:43:26 +0900

    feat: impl system selector

--- flake.nix
+++ flake.nix
@@ -3,32 +3,48 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs: {
-    nixosConfigurations.laptop = {
-      modules = [
-        ./hardware-configuration.nix
-        (import ./profile "desktop")
-        (import ./app [
+  outputs =
+    { nixpkgs }:
+    let
+      lib = nixpkgs.lib;
+      makeSystem =
+        {
+          profile,
+          device,
+          apps,
+          users,
+        }:
+        lib.nixosSystem {
+          modules = [
+            ./hardware-configuration.nix
+            (import ./profile profile)
+            (import ./dev device)
+            (import ./app apps)
+            (import ./user users)
+          ];
+        };
+    in
+    {
+      nixosConfigurations.laptop = makeSystem {
+        profile = "desktop";
+        device = "LEN-16AFR10-83F2";
+        apps = [
           "game"
           "git"
           "nvim"
           "zed"
-        ])
-        (import ./user [
-          "sharp0802"
-        ])
-      ];
-    };
-    nixosConfigurations.server = {
-      modules = [
-        ./hardware-configuration.nix
-        (import ./profile "server")
-        (import ./app [ ])
-        (import ./user [
+        ];
+        users = [ "sharp0802" ];
+      };
+
+      nixosConfigurations.server = makeSystem {
+        profile = "server";
+        device = "MSI-CX62-6QD";
+        apps = [ ];
+        users = [
           "sharp0802"
           "seocaf2"
-        ])
-      ];
+        ];
+      };
     };
-  };
 }
