07ae76c9a324dd94f317dd39e90b4da351392544
sharp0802 <sharp0802.business@gmail.com>
2026-07-13 13:39:07 +0900

    fix: fix gpu bus ids

--- dev/LEN-16AFR10-83F2.nix
+++ dev/LEN-16AFR10-83F2.nix
@@ -15,13 +15,18 @@
         enable = true;
         enableOffloadCmd = true;
       };
-      amdgpuBusId = "PCI:7:0:0";
-      nvidiaBusId = "PCI:1:0:0";
+      amdgpuBusId = "PCI:7@0:0:0";
+      nvidiaBusId = "PCI:1@0:0:0";
     };
   };
 
+  services.udev.extraRules = ''
+    SUBSYSTEM=="drm", KERNEL=="card[0-9]*", KERNELS="0000:07:00.0", SYMLINK+="dri/wlr-1"
+    SUBSYSTEM=="drm", KERNEL=="card[0-9]*", KERNELS="0000:01:00.0", SYMLINK+="dri/wlr-2"
+  '';
+
   environment.variables = {
-    WLR_DRM_DEVICES = "/dev/dri/by-path/pci-0000:07:00.0-card";
+    WLR_DRM_DEVICES = "/dev/dri/wlr-1:/dev/dri/wlr-2";
   };
 
   services.xserver.videoDrivers = [
--- dev/MSI-CX62-6QD.nix
+++ dev/MSI-CX62-6QD.nix
@@ -10,13 +10,18 @@
         enable = true;
         enableOffloadCmd = true;
       };
-      intelBusId = "PCI:0:0:2";
-      nvidiaBusId = "PCI:1:0:0";
+      intelBusId = "PCI:0@0:2:0";
+      nvidiaBusId = "PCI:1@0:0:0";
     };
   };
+  
+  services.udev.extraRules = ''
+    SUBSYSTEM=="drm", KERNEL=="card[0-9]*", KERNELS="0000:00:02.0", SYMLINK+="dri/wlr-1"
+    SUBSYSTEM=="drm", KERNEL=="card[0-9]*", KERNELS="0000:01:00.0", SYMLINK+="dri/wlr-2"
+  '';
 
   environment.variables = {
-    WLR_DRM_DEVICES = "/dev/dri/by-path/pci-0000:00:02.0-card";
+    WLR_DRM_DEVICES = "/dev/dri/wlr-1:/dev/dri/wlr-2";
   };
 
   services.xserver.videoDrivers = [
