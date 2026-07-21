0ce852768190e2d4f6041d7f2b73197e7acac4ea
sharp0802 <sharp0802.business@gmail.com>
2026-07-21 21:36:49 +0900

    fix: update oo7 to resolve login issue

--- profile/shared/overlay/oo7.nix
+++ profile/shared/overlay/oo7.nix
@@ -1,19 +1,29 @@
-final: _prev:
+final: prev:
 let
+  version = "0.7.0-alpha";
+  hash = "sha256-JshJPQgV8+LgKPflpxE9r8uwsDap9EWR/Z5sGY0sEao=";
+  cargoHash = "sha256-IPDOdCJuSJiNCWaAPGV6c9WcvIaQITmtEPfB+lza8ro=";
+
+  src = final.fetchFromGitHub {
+    owner = "linux-credentials";
+    repo = "oo7";
+    rev = "0.7.0.alpha";
+    inherit hash;
+  };
+
+  cargoDeps = final.rustPlatform.fetchCargoVendor {
+    inherit src;
+    hash = cargoHash;
+  };
+
+  oo7 = prev.oo7.overrideAttrs (_old: {
+    inherit version src cargoDeps;
+  });
+
   mkComponent =
     { pname }:
     final.rustPlatform.buildRustPackage {
-      inherit pname;
-      version = "main";
-
-      src = final.fetchFromGitHub {
-        owner = "linux-credentials";
-        repo = "oo7";
-        rev = "main";
-        hash = "sha256-AIvGbfmN0fUsm63kFO8WETUmuR1YTWLwMKuaxRL7eho=";
-      };
-
-      cargoHash = "sha256-UFExipKWK0BSBbQTDT5h09XyeH4NIeWVjlUpS4SNvdA=";
+      inherit pname version src cargoHash;
 
       RUSTFLAGS = "-C target-cpu=native";
       CARGO_PROFILE_RELEASE_LTO = "true";
@@ -30,6 +40,18 @@ let
     };
 in
 {
+  inherit oo7;
+
+  oo7-pam = prev.oo7-pam.override {
+    inherit oo7;
+  };
+  oo7-server = prev.oo7-server.override {
+    inherit oo7;
+  };
+  oo7-portal = prev.oo7-portal.override {
+    inherit oo7;
+  };
+
   git-credential-oo7 = mkComponent {
     pname = "git-credential-oo7";
   };
