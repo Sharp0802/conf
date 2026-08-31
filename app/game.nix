08dd53a84bc9487dae28910ab4e1c2fae3e7207a
sharp0802 <sharp0802.business@gmail.com>
2026-08-31 09:37:28 +0900

    feat: add opencl support for prismlauncher

--- app/game.nix
+++ app/game.nix
@@ -1,6 +1,15 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [
-    prismlauncher
+    (symlinkJoin {
+      name = "prismlauncher-opencl";
+      paths = [ prismlauncher ];
+      nativeBuildInputs = [ makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/prismlauncher \
+          --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath [ pkgs.ocl-icd ]}:/run/opengl-driver/lib" \
+          --set OCL_ICD_VENDORS /run/opengl-driver/etc/OpenCL/vendors
+      '';
+    })
     r2modman
   ];
 
