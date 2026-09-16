7db9e8610e7eac750dadf3222eed75bf9d960f10
sharp0802 <sharp0802.business@gmail.com>
2026-09-16 12:38:40 +0900

    fix: fix FHS env of unity

--- app/unity.nix
+++ app/unity.nix
@@ -7,6 +7,14 @@ let
     extraPkgs = p: [
       p.ncurses
     ];
+    buildFHSEnv = args:
+      pkgs.buildFHSEnv (args // {
+        profile = (args.profile or "") + ''
+          export LD_LIBRARY_PATH="${
+            pkgs.lib.makeLibraryPath [ pkgs.ncurses ]
+          }''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+        '';
+      });
   };
 in
 {
